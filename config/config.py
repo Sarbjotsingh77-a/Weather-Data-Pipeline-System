a265c467c118a27623acbb648714c85e6db06405
Sarbjotsingh77-a <kherarajindersingh94@gmail.com>
2026-06-08 21:05:50 +0530

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,19 @@
+API_KEY = "YOUR_OPENWEATHERMAP_API_KEY"
+
+DATABASE_NAME = "database/weather_data.db"
+
+TRACKED_CITIES = [
+    "Mumbai",
+    "Delhi",
+    "Bangalore",
+    "Chennai",
+    "Kolkata"
+]
+
+TEMPERATURE_ALERT_THRESHOLD = 30
+
+HUMIDITY_ALERT_THRESHOLD = 75
+
+LOG_FILE = "logs/pipeline.log"
+
+ERROR_LOG_FILE = "logs/error.log"
